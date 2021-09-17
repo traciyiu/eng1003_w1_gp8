559e6dae4c46bfb709204512863ad35d25b734d2
YZ0031 <90884775+YZ0031@users.noreply.github.com>
2021-09-17 14:56:37 +0800

    Update w1.py
    Added by Cheung Pong Yuen

--- w1.py
+++ w1.py
@@ -1,1 +1,17 @@
 input your ans in here
+
+def var_comp():
+
+    a = 5
+    b = 4
+
+    print(f"a = {a} b = {b}")
+
+    if a > b:
+        print("a is larger than b")
+
+    elif a < b:
+        print("a is smaller than b")
+
+    else:
+        print("a and b are equal")
