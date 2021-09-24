b88af7c496fc59538646ed52e05cf2c37b60dbd8
traciyiu <90884975+traciyiu@users.noreply.github.com>
2021-09-24 11:34:46 +0800

    Update w1.py

--- w1.py
+++ w1.py
@@ -2,4 +2,9 @@ from _typeshed import HasFileno
 
 
 input your ans in here
-Hi
+def put2():
+    print("enter the function number to be excuted:")
+    a=input("The first number:")
+    b=input("The second number:")
+    sum=int(a)+int(b)
+    print('The sum is:'+str(sum))
