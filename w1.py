44f9eb3fe1043c1a1e9f02f5b0f091954a22fc4f
jayyy718 <90884238+jayyy718@users.noreply.github.com>
2021-09-24 11:29:37 +0800

    Update w1.py

--- w1.py
+++ w1.py
@@ -1,8 +1,8 @@
 input your ans in here
 def put1():
-    j = Jay
-    t = Traci
-    z = Zhe
-    y = Jackie
-    l = LiTaiZong
-print("j"\"t"\"z"\"y"\"l")
+    j="Jay"
+    t="Traci"
+    z="Zhe"
+    y="Jackie"
+    l="LiTaiZong"
+    print(j,"\n",t,"\n",z,"\n",y,"\n",l)
