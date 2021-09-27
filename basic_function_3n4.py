613233473b60c1ba6fd742d6a65fc89345c32d2d
YZ0031 <90884775+YZ0031@users.noreply.github.com>
2021-09-27 21:46:03 +0800

    Add files via upload

---
+++ basic_function_3n4.py
@@ -0,0 +1,22 @@
+def function_3():
+
+    a = 5
+
+    print("a = 5 , Square of a:",a*a)
+
+def function_4():
+    
+    a = 5
+    b = 4
+
+    print(f"a = {a} b = {b}")
+
+    if a > b:
+        print("a is larger than b")
+
+    elif a < b:
+        print("a is smaller than b")
+
+    else:
+        print("a and b are equal")
+
